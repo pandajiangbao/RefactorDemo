03709448cdedf08fa84462c555b60b7f29334fbb
WUPA2 <panda.wu@oocl.com>
2019-08-12 23:00:44 +0800

    optimize structure

--- src/main/java/gildedRose/Item.java
+++ src/main/java/gildedRose/Item.java
@@ -14,8 +14,8 @@ public class Item {
         this.quality = quality;
     }
 
-    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
     public static final String AGED_BRIE = "Aged Brie";
+    public static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
     public static final String SULFURAS_HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 
     @Override
